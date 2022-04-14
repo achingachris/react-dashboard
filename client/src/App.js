6a6e1c8a4fc6718dd2654f46791e393a4df5273e
chrisachinga <achinga.chris@gmail.com>
2022-04-14 13:06:45 +0300

    home-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import Layout from './layout/Layout'
+import PageHeading from './components/global/PageHeading'
+import SummaryCards from './components/global/SummaryCards'
+
+import Charts from './components/home/Charts'
+import Content from './components/home/Content'
 
 const App = () => {
-  return <Layout>App</Layout>
+  return (
+    <Layout>
+      <PageHeading title='Dashboard' />
+      <SummaryCards />
+      <Charts />
+      <Content />
+    </Layout>
+  )
 }
 
 export default App
